aaff401fea10a7c01db81a43858895fae49cddd7
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-19 23:38:30 +0300

    Factor out the file processing code into get_athlete_data()

--- track_coach.py
+++ track_coach.py
@@ -2,8 +2,7 @@
 """ Track Coach Data 
 A play on files and working the data files, 
 text files holding athletes track records.
-    * Process the data from file into lists
-    * Transform and display data in the lists.
+    * Read from file, transform the data and process into sorted lists
     * Display top three fastest times for each athlete
 """
 
@@ -29,33 +28,28 @@ def sanitize(time_string):
 
 # Process each file, creating a list for each athlete’s data
 
-# TODO:
-# factor out the code into a small function; 
-# call the function for each of the ayhletes data files, 
-# assigning the result to athletes' list.
+def get_athlete_data(filename):
+    """ (file) -> list
+    Takes <filename>, the file with the athletes' track records.
+    Processes each file, creating a list for each athlete’s data.
+    Returns the list, otherwise, an error msg and None
+    """
+    try:
+        with open(filename) as file_obj:    # Open the file
+            file_data = file_obj.readline() # read the data
+        return file_data.strip().split(',') # format the data & return to calling code
 
-try:
-    with open('text/james.txt') as james_file:  # Open file & assign  file object
-        james_data = james_file.readline()      # Read the line of data
-        james = james_data.strip().split(',')   # convert data into list
+    except IOError as ioerr:
+        print('File Error...', ioerr)
+        return None
 
-    with open('text/julie.txt') as julie_file:
-        julie_data = julie_file.readline()      # Read the line of data
-        julie = julie_data.strip().split(',')   # convert data into list
+james = get_athlete_data('text/james.txt')
+julie = get_athlete_data('text/julie.txt')
+sarah = get_athlete_data('text/sarah.txt')
+mikey = get_athlete_data('text/mikey.txt')
 
-    with open('text/mikey.txt') as mikey_file:
-        mikey_data = mikey_file.readline()      # Read the line of data
-        mikey = mikey_data.strip().split(',')   # convert data into list
-
-    with open('text/sarah.txt') as sarah_file:
-        sarah_data = sarah_file.readline()      # Read the line of data
-        sarah = sarah_data.strip().split(',')   # convert data into list
-
-except IOError as ioerr:
-    print('File Error...', ioerr)
-
-# Remove duplicates and display the top3 athletes' records 
-print('\n')
+# Display the top3 athletes' records without duplicates
+print()
 print('Top 3 - Three fastest times for each athlete')
 print()
 print('James: {0}'.format(sorted(set([sanitize(each_time) for each_time in james]))[0:3]))
